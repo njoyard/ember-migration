d63cf2d3e34116971ffa44963bc84996d533e697
Nicolas Joyard <nicolas.joyard@mainmatter.com>
2026-06-02 14:34:27 +0200

    chore: app title

---
+++ tests/acceptance/title-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | title', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+
+    assert.dom('h1').containsText('TODO');
+  });
+});
